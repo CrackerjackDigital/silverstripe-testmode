62a4591db149e25792c389b76fe48b1229df316c
Steve Wake <steve@moveforward.co.nz>
2017-08-21 22:07:06 +1200

    from delectus

--- _config.php
+++ _config.php
@@ -3,9 +3,9 @@
  * Enable testing using sqlite3 in-memory database if we're not in Live mode and url begins with /dev/tests
  */
 if (!Director::isLive()) {
-	if ((isset($_REQUEST['url']) && substr($_REQUEST['url'], 0, 12) == '/dev/tests/')) {
+	if ((isset($_REQUEST['url']) && trim(substr($_REQUEST['url'], 0, 10), '/') == 'dev/tests')) {
 		global $databaseConfig;
-		$databaseConfig['SS_DATABASE_TYPE'] = 'SQLite3Database';
-		$databaseConfig['SS_DATABASE_PATH'] = ':memory:';
+		$databaseConfig['type'] = 'SQLite3Database';
+		$databaseConfig['path'] = ':memory:';
 	}
 }
